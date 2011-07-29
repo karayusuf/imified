ca5e2466ffcfe7cf7b359d9ec3bfe464d6ce67b4
Oatmeal <jkarayusuf@medsagetechnologies.com>
2011-07-29 00:18:53 -0400

    Added username, password, and imified_url constants.

--- lib/imified.rb
+++ lib/imified.rb
@@ -1,7 +1,22 @@
-require "imified/version"
+require 'net/http'
+require 'uri'
 
 module Imified
   autoload :Message, 'imified/message'
+  autoload :Version, 'imified/version'
 
-  BOTKEY = YAML.load_file('lib/imified.yml')["Imified"]["botkey"]
+  # Identify the Imified configuration.
+  CONFIG = YAML.load_file('lib/imified.yml')
+
+  # Reference to the Imified BOTKEY.
+  BOTKEY = CONFIG["Imified"]["botkey"]
+
+  # Reference to the Imified Account Username.
+  USERNAME = CONFIG["Imified"]["username"]
+
+  # Reference to the Imified Account Password.
+  PASSWORD = CONFIG["Imified"]["password"]
+
+  # Imified API URL.
+  IMIFIED_URL = URI.parse('https://www.imified.com/api/bot/')
 end
--- spec/imified_spec.rb
+++ spec/imified_spec.rb
@@ -4,4 +4,16 @@ describe Imified do
   it "should know the users bot key" do
     Imified::BOTKEY.should_not be_nil
   end
+
+  it "should know the account name" do
+    Imified::USERNAME.should_not be_nil
+  end
+
+  it "should know the account password" do
+    Imified::PASSWORD.should_not be_nil
+  end
+
+  it "should know the Imified URL" do
+    Imified::IMIFIED_URL.should eql(URI.parse('https://www.imified.com/api/bot/'))
+  end
 end
