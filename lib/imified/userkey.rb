757462f4aa6b06940974e2942dc3bb2e106bc40e
Joe Karayusuf <jkarayusuf@gmail.com>
2011-11-02 23:37:19 -0400

    Refactored userkey lookup

--- lib/imified/userkey.rb
+++ lib/imified/userkey.rb
@@ -1,13 +1,24 @@
 class Imified::Userkey
-
   def self.find(recipient)
-    Imified.user_list ||= Imified.get_all_users
-
     if recipient.include?('@')
-      Imified.user_list.xpath("//user/screenname[text() = '#{recipient}']/../userkey/text()").map(&:to_s).join(',')
+      find_userkey_by_email(recipient)
     else
       recipient
     end
+  end
+
+  private
+
+  def self.find_userkey_by_email(email, attempt = 1)
+    Imified.user_list ||= Imified.get_all_users
+    recipient = Imified.user_list.xpath("//user/screenname[text() = '#{email}']/../userkey/text()").map(&:to_s).join(',')
+    recipient = update_users_and_try_again(email) if recipient.nil? && attempt == 1
+    recipient
+  end
+
+  def self.update_users_and_try_again(email)
+    Imified.get_all_users
+    self.find_userkey_by_email(email, 2)
   end
 
 end
